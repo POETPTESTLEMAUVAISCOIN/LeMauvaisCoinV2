fc418298f29b0e87bdd5d98c4e16d73686a17e6b
Geoffrey CABASSON <geoffrey.cabasson@neuf.fr>
2023-02-20 09:43:18 +0100

    first commit

---
+++ com.LeMauvaisCoin/src/main/java/com/LeMauvaisCoin/com/LeMauvaisCoin/LeMauvaisCoinApplication.java
@@ -0,0 +1,13 @@
+package com.LeMauvaisCoin.com.LeMauvaisCoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeMauvaisCoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeMauvaisCoinApplication.class, args);
+	}
+
+}
---
+++ com.LeMauvaisCoin/src/test/java/com/LeMauvaisCoin/com/LeMauvaisCoin/LeMauvaisCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LeMauvaisCoin.com.LeMauvaisCoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeMauvaisCoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
